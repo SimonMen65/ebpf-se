a3911e391080af480f221597a09722c08f5c98c0
SimonMen65 <jm7828@nyu.edu>
2024-04-29 23:21:51 -0400

    extend katran

--- examples/katran/bpf_helpers.h
+++ examples/katran/bpf_helpers.h
@@ -77,11 +77,12 @@ void bpf_map_init_stub(struct bpf_map_def *map, char *name, char *key_type, char
   return;
 }
 
-void bpf_map_of_maps_init_stub(struct bpf_map_def* outer, struct bpf_map_def* inner, char *name, char *key_type, char * val_type){
+void bpf_map_of_maps_init_stub(struct bpf_map_def* outer, struct bpf_map_def* inner, char *outer_name, char *name, char *key_type, char * val_type){
   // klee_trace_ret();
   outer->map_id = bpf_map_ctr++;
   assert(outer->type == 12 || outer->type == 13);
-  bpf_map_stubs[outer->map_id] = map_of_map_allocate(inner,bpf_map_ctr);
+  bpf_map_stubs[outer->map_id] = map_of_map_allocate(outer_name,inner,bpf_map_ctr);
+  //bpf_map_stubs[outer->map_id] = map_of_map_allocate(inner,bpf_map_ctr);
   bpf_map_stub_types[outer->map_id] = MapofMapStub;
   assert(inner->type == 1 || inner->type == 5 || inner->type == 9);
   bpf_map_stubs[bpf_map_ctr] = map_allocate(name, key_type, val_type, inner->key_size, inner->value_size, inner->max_entries);
